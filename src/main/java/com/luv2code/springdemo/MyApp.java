b54c3cd3f2a9d7fd9407f624ed3990e5a426b4cf
thamhv <learnwiththamhv@gmail.com>
2019-09-14 15:08:32 +0700

    dependency injection. closes #3

--- src/main/java/com/luv2code/springdemo/MyApp.java
+++ src/main/java/com/luv2code/springdemo/MyApp.java
@@ -1,12 +1,14 @@
 package com.luv2code.springdemo;
 
 public class MyApp {
-    public static void main(String[] args) {
 
-        // create object
-        Coach theCouch = new TrackCoach();
+	public static void main(String[] args) {
 
-        // use the object
-        System.out.println(theCouch.getDailyWorkout());
-    }
+		// create the object
+		Coach theCoach = new TrackCoach();
+		
+		// use the object
+		System.out.println(theCoach.getDailyWorkout());		
+	}
+
 }
